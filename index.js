f84699898f48d01c58a0958e94d99fe528f6cf9c
Ethan Freeman <ethanfree23@gmail.com>
2021-11-23 10:16:43 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(ride) {
+  if (ride>2500) {
+    return "No can do."
+  } else if (ride>2000) {
+    return "I will gladly take your thirty bucks."
+  } else if (ride<=400) {
+    return "This one is on me!"
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city = "NYC"
+function ternaryCheckCity(city) {
+  return (city==="NYC" ? "Ok, sounds good." : "No go."); 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case `generous`:
+      return "Thank you so much.";
+    case `not as generous`:
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
+
+
+
+
+//   switch(tip) {
+//     case "generous":
+//       return "Thank you so much."
+//       break;
+//     case "not as generous":
+//       return "Thank you."
+//       break;
+//     case (null):
+//       return "Bye."
+//       break;
+//   }
+// }
